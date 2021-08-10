46c38bbfd3504ab28e34211ab0391910f049105d
Rodrigo Rodriguez <me@rodrigorodriguez.com>
2021-08-10 14:57:03 -0300

    new(basic.gblib) New SET SCHEDULE keyword.

--- packages/basic.gblib/strings.ts
+++ packages/basic.gblib/strings.ts
@@ -1,9 +1,9 @@
 
 export const Messages = {
   'en-US': {
-
+    affirmative_sentences: /^(\bsim\b|\bs\b|\bpositivo\b|\bafirmativo\b|\bclaro\b|\bevidente\b|\bsem dúvida\b|\bconfirmo\b|\bconfirmar\b|\bconfirmado\b|\buhum\b|\bsi\b|\by\b|\byes\b|\bsure\b)/i
   },
   'pt-BR': {
-
+    affirmative_sentences: /^(\bsim\b|\bs\b|\bpositivo\b|\bafirmativo\b|\bclaro\b|\bevidente\b|\bsem dúvida\b|\bconfirmo\b|\bconfirmar\b|\bconfirmado\b|\buhum\b|\bsi\b|\by\b|\byes\b|\bsure\b)/i
   }
 };
