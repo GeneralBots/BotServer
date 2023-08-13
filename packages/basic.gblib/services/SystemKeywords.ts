3fb4d814ddd39319a1d72968e1c932f1f07595f1
Rodrigo Rodriguez <me@rodrigorodriguez.com>
2023-08-13 18:42:50 -0300

    fix(basic.gbapp): AS IMAGE testing.

--- packages/basic.gblib/services/SystemKeywords.ts
+++ packages/basic.gblib/services/SystemKeywords.ts
@@ -251,7 +251,7 @@ export class SystemKeywords {
 
     // Includes the associated CSS related to current theme.
 
-    const theme: string =  await DialogKeywords.getOption({ pid, name: 'theme' });
+    const theme: string =  await DialogKeywords.getOption({ pid, name: 'theme', root: true });
     switch (theme) {
       case 'white':
         await page.addStyleTag({ path: 'node_modules/tabulator-tables/dist/css/tabulator_simple.min.css' });
