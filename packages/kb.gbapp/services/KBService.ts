0fc3d714c044034603776b4b2da2b92046e8826e
Rodrigo Rodriguez <me@rodrigorodriguez.com>
2023-09-13 19:37:04 -0300

    fix(kb.gbapp): Fix importing packages.

--- packages/kb.gbapp/services/KBService.ts
+++ packages/kb.gbapp/services/KBService.ts
@@ -1020,7 +1020,7 @@ export class KBService implements IGBKBService {
     const instance = await core.loadInstanceByBotId(min.botId);
     GBLog.info(`[GBDeployer] Importing: ${localPath}`);
     
-    const p = await deployer.undeployPackageFromLocalPath(instance, packageName);
+    await deployer.undeployPackageFromLocalPath(instance, packageName);
     
     const p = await deployer.deployPackageToStorage(instance.instanceId, packageName);
     await this.importKbPackage(min, localPath, p, instance);
