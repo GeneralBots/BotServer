b0aeb83d2ed156a93ceb5dbe3a0630516169f2e5
Rodrigo Rodriguez <rodriguez@pragmatismo.cloud>
2024-08-10 11:40:36 -0300

    new(whatsapp.gblib): Auto-create WhatsApp templates from articles in .docx.

--- packages/basic.gblib/services/KeywordsExpressions.ts
+++ packages/basic.gblib/services/KeywordsExpressions.ts
@@ -1176,6 +1176,7 @@ export class KeywordsExpressions {
       /^\s*MERGE\s*(.*)\s*WITH\s*(.*)BY\s*(.*)/gim,
       ($0, $1, $2, $3) => {
         return `__reportMerge1 = await sys.merge({pid: pid, file: ${$1}, data: ${$2}, key1: ${$3}})
+        ${$2} = null;
         __reportMerge.adds += __reportMerge1.adds;
         __reportMerge.updates += __reportMerge1.updates;
         __reportMerge.skipped += __reportMerge1.skipped;
@@ -1297,6 +1298,7 @@ export class KeywordsExpressions {
         }else{
           await sys.saveToStorage({pid: pid, table: ${table}, fieldsValues: [${fieldsAsText}], fieldsNames: [${fieldsNames}] })
         }
+        ${table} = null;
       `;
       }
     ];
