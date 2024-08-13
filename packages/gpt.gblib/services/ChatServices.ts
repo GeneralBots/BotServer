7b9d7446da986903def74d390bd3161f32f2d5fe
Rodrigo Rodriguez <rodriguez@pragmatismo.cloud>
2024-08-13 20:48:53 -0300

    new(whatsapp.gblib): LLM SQL.

--- packages/gpt.gblib/services/ChatServices.ts
+++ packages/gpt.gblib/services/ChatServices.ts
@@ -537,7 +537,7 @@ export class ChatServices {
       const finalResponsePrompt = SQL_SQLITE_PROMPT
         PromptTemplate.fromTemplate(`Based on the table schema below, question, SQL query, and SQL response, write a natural language response:
           You are a SQLite expert. Given an input question, first create a syntactically correct SQLite query to run, then look at the results of the query and return the answer to the input question.
-Unless the user specifies in the question a specific number of examples to obtain, query for at most {top_k} results using the LIMIT clause as per SQLite. You can order the results to return the most informative data in the database.
+Unless the user specifies in the question a specific number of examples to obtain, query for at most results using the LIMIT clause as per SQLite. You can order the results to return the most informative data in the database.
 Never query for all columns from a table. You must query only the columns that are needed to answer the question. Wrap each column name in double quotes (") to denote them as delimited identifiers.
 Pay attention to use only the column names you can see in the tables below. Be careful to not query for columns that do not exist. Also, pay attention to which column is in which table.
 
