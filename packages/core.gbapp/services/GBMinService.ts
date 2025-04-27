320e401cadf25d88f09d73c3e1caa23d52eb8bd6
Rodrigo Rodriguez (Pragmatismo) <me@rodrigorodriguez.com>
2025-04-27 19:00:54 -0300

    fix(GBMinService): update CORS origin to use HTTPS for improved security

--- packages/core.gbapp/services/GBMinService.ts
+++ packages/core.gbapp/services/GBMinService.ts
@@ -166,7 +166,7 @@ export class GBMinService {
       // Servers the bot information object via HTTP so clients can get
       // instance information stored on server.
       GBServer.globals.server.use(cors({
-        origin: 'gb6.pragmatismo.com.br',
+        origin: 'https://gb6.pragmatismo.com.br',
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
         allowedHeaders: ['Content-Type', 'Authorization', 'x-requested-with'
           , 'x-ms-bot-agent'
