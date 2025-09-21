30663a608e151894866962e304e95acfa1d277d5
Rodrigo Rodriguez (Pragmatismo) <rodriguez@pragmatismo.com.br>
2025-09-21 16:37:58 -0300

    - Warning removal.

---
+++ install-addons.sh
@@ -0,0 +1,9 @@
+npm --save install  \
+    c3-chart-maker@0.2.8 \
+
+    open-docxtemplater-image-module@1.0.3 \
+    svg2img@1.0.0-beta.2 \
+    get-image-colors@4.0.1 \
+
+    pragmatismo-framework \
+    botlib \
